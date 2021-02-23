eadba95f22548353a66d261492dddd8c9f1f96ad
huangzefeng <737244264@qq.com>
2021-02-23 21:09:18 +0800

    二叉排序树的中序遍历输出是一个递增序列

--- search/search.py
+++ search/search.py
@@ -18,9 +18,76 @@ def bi_search(arr, num):
         else:
             return mid
 
-    return -1 
+    return -1
+
+
+# -----------------------------------------------------
+# 2. 二叉排序树建立
+#              45
+#             /  \
+#            12  53
+#           / \    \
+#          3  37   100
+#            /      /
+#          24      61
+#                   \
+#                   90
+#                   /
+#                  78
+# ------------------------------------------------------
+
+class BiTreeNode():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def create_bi_sorted_tree():
+    root = BiTreeNode(45)
+    node1 = BiTreeNode(12)
+    node2 = BiTreeNode(3)
+    node3 = BiTreeNode(37)
+    node4 = BiTreeNode(24)
+
+    node5 = BiTreeNode(53)
+    node6 = BiTreeNode(100)
+    node7 = BiTreeNode(61)
+    node8 = BiTreeNode(90)
+    node9 = BiTreeNode(78)
+
+    root.left = node1
+    node1.left = node2
+    node1.right = node3
+    node3.left = node4
+
+    root.right = node5
+    node5.right = node6
+    node6.left = node7
+    node7.right = node8
+    node8.left = node9
+
+    return root
+
+
+# -----------------------------------------------------
+# 2. 二叉排序树的中序遍历
+# -----------------------------------------------------
+
+def mid_visit_tree(root):
+
+    if not root:
+        return
+    
+    mid_visit_tree(root.left)
+    print(root.data)
+    mid_visit_tree(root.right)
+
+
+def mid_visit_bi_sorted_tree():
+    root = create_bi_sorted_tree()
+    mid_visit_tree(root)
 
 
 if __name__ == '__main__':
-    index = bi_search([1, 4, 6, 8, 12, 34, 66], 11)
-    print(index)
+    mid_visit_bi_sorted_tree()
