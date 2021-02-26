51b9333ed00822d90d11a0e8e7e0511489e5658f
huangzefeng <737244264@qq.com>
2021-02-26 22:54:48 +0800

    折半插入排序

--- sort/sort.py
+++ sort/sort.py
@@ -15,8 +15,40 @@ def insert_sort(num_list):
 
     return num_list
 
+
+# ------------------------------------------------------
+# 2. 折半插入排序
+# ------------------------------------------------------
+
+
+def bi_insert_sort(num_list):
+    for i in range(len(num_list)):
+        tmp = num_list[i]
+
+        left = 0
+        right = i - 1
+        copy_right = right
+        while left <= right:
+            mid = (left + right) // 2
+
+            if tmp > num_list[mid]:
+                left = mid + 1
+            elif tmp < num_list[mid]:
+                right = mid - 1
+            else:
+                break
+        
+        while copy_right >= left:
+            num_list[copy_right+1] = num_list[copy_right]
+            copy_right -= 1
+        
+        num_list[left] = tmp
+
+    return num_list
+
+
 if __name__ == '__main__':
-    num_list = [4, 2, 5, 7, 34, 3, 25, 67]
-    sort_l = insert_sort(num_list)
+    num_list = [4, 2, 5, 7, 34, 3, 25, 67, 12]
+    sort_l = bi_insert_sort(num_list)
     print(sort_l)
     
